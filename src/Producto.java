92863785bbc35e525f6cb40bbdd881da85c49f77
Alberto Pastor <apastor63@hotmail.com>
2026-05-14 20:56:39 -0300

    update de prueba

---
+++ src/Producto.java
@@ -0,0 +1,80 @@
+package com.techlab.ecommerce.model;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String categoria;
+    private boolean activo;
+
+    public Producto(String nombre, double precio, int stock, String categoria, boolean activo){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+        this.activo = activo;
+    }
+
+    public Producto(){
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString(){
+        return "ID: " + id +
+                " | " + nombre + 
+                " | $ " + precio +
+                " | Stock: " + stock +
+                " | Categoria: " + categoria +
+                " | Activo: " + activo;
+    }
+}
